b3b1f0f1aa7374184756d11f3ae0dbf04d96c118
paopao <lubingjie1@360.cn>
2019-11-07 11:00:39 +0800

    添加导出模块

--- projects/layout-menus/src/public-api.ts
+++ projects/layout-menus/src/public-api.ts
@@ -3,3 +3,6 @@
  */
 
 export * from './lib/layout-menus.module';
+export * from './lib/layout-side/layout-header.component';
+export * from './lib/layout-side/layout-side.component';
+export * from './lib/layout-side/layout.interface';
